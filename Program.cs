2f7fc707c0a9a071a2313ba541551f9ce7a51f0a
MrSinon341 <144006580+MrSinon341@users.noreply.github.com>
2023-09-04 12:15:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PR_New_9._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Задача:");
+            Console.WriteLine(" С клавиатуры вводится строка S. Вывести строку, содержащую символы строки S ");
+            Console.WriteLine(" между которыми вставлено по одному пробелу.");
+
+            Console.WriteLine(); // отступ абзац;
+
+            string text = ""; // резеврируем место под переменную;
+                       
+            Console.Write(" Введите стоку S: ");
+            text = Console.ReadLine();
+                        
+            Console.WriteLine(); // отступ абзац;
+
+            Console.Write(" Результат выполненного задания: ");
+
+            char[] ch = text.ToCharArray();
+
+            foreach (char c in ch)
+            {
+               Console.Write(c + " ");
+            }
+                           
+            Console.WriteLine(); // отступ абзац;
+                        
+        }
+    }
+}
